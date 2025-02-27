06674688d4ec18eba93f10b41e7ecba31346f5e6
Hiren Patel <hirenp231994@gmail.com>
2025-02-27 00:07:29 -0500

    added user input to be sent to server which response back will be the same message

--- src/ClientApplication.java
+++ src/ClientApplication.java
@@ -1,6 +1,6 @@
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ClientApplication {
     public static void main(String[] args) {
@@ -8,9 +8,20 @@ public class ClientApplication {
 
 
         try (Socket socket = new Socket("localhost",8080)){
+            InputStream is = socket.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
             OutputStream os = socket.getOutputStream();
-            os.write("hello from client".getBytes());
-            os.flush();
+            PrintWriter printWriter = new PrintWriter(os,true);
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter message: ");
+            while (sc.hasNext()) {
+                String message = sc.nextLine();
+                if (message.equals("exit")) break;
+                printWriter.println(message);
+                String messageFromServer = br.readLine();
+                System.out.println("From Server:" + messageFromServer);
+                System.out.print("Enter message: ");
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
